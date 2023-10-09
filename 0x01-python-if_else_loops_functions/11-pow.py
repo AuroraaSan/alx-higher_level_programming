27911412171c74071aa5fadea3cf9af0ce238e27
AuroraaSan <arwa.zakaria@ejust.edu.eg>
2023-10-09 11:55:57 +0300

    kk

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 def pow(a, b):
-    return a ^ b
+    if b == 0:
+        return 1
+    res = a
+    for i in range (1, abs(b)):
+        res *= a
+    if b < 0:
+        return 1 / res
+    return res
