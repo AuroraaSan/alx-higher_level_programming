3b548c7634b6c14ecc11dd24c5d3375631dad620
AuroraaSan <arwa.zakaria@ejust.edu.eg>
2023-10-10 21:08:25 +0300

    rr

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,7 +2,7 @@
 if "__name__" == "__main__":
     import sys
     result = 0
-    count = len(sys.arg) - 1
+    count = len(sys.argv) - 1
     for i in range(count):
         result += int(sys.argv[i + 1])
     print(result)
