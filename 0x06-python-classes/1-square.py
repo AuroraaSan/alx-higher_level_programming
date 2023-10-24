b990fd2e130ef87bf57c9688d81184f0063bee91
AuroraaSan <arwa.zakaria@ejust.edu.eg>
2023-10-24 20:17:02 +0300

    task1 again

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
-Square = __import__('0-square').Square
+Square = __import__('1-square').Square
 
 my_square = Square(3)
 print(type(my_square))
 print(my_square.__dict__)
 
 try:
-    print(my_square.size)
+    print(my_square._Square__size)  # Access private attribute with name mangling
 except Exception as e:
     print(e)
 
 try:
-    print(my_square.__size)
+    print(my_square.size)  # This should raise an exception
+except Exception as e:
+    print(e)
+
+try:
+    print(my_square.__size)  # This should raise an exception
 except Exception as e:
     print(e)
