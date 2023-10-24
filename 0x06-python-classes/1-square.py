e4e0432d50adac5eca6931b501a6e621dbbe60ce
AuroraaSan <arwa.zakaria@ejust.edu.eg>
2023-10-24 20:24:40 +0300

    task

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 
 class Square():
+    """ defines a square """
     def __init__(self, size):
+        """constructor
+
+        Args:
+            size: length of the side of the square
+        """
         self.__size = size
