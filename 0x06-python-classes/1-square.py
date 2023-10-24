75326edca98e77d0efd2a0bad23492d449bb5ef0
AuroraaSan <arwa.zakaria@ejust.edu.eg>
2023-10-24 20:22:21 +0300

    task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,21 +1,5 @@
 #!/usr/bin/python3
-Square = __import__('1-square').Square
 
-my_square = Square(3)
-print(type(my_square))
-print(my_square.__dict__)
-
-try:
-    print(my_square._Square__size)  # Access private attribute with name mangling
-except Exception as e:
-    print(e)
-
-try:
-    print(my_square.size)  # This should raise an exception
-except Exception as e:
-    print(e)
-
-try:
-    print(my_square.__size)  # This should raise an exception
-except Exception as e:
-    print(e)
+class Square():
+    def __init__(self, size):
+        self.__size = size
