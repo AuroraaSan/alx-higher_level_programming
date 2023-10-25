d3527fc7b068ef892b1a5e15cfe0c152b037ad35
AuroraaSan <arwa.zakaria@ejust.edu.eg>
2023-10-25 20:25:44 +0300

    re

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -6,9 +6,4 @@ class Square:
     """Class to represent a square."""
 
     def __init__(self, size):
-        """Initializes a new Square instance.
-
-        Args:
-            size (int): The size of the square.
-        """
         self.__size = size
