9fc5fd6b29e53dde17e8299cf856e242daa8b37a
AuroraaSan <arwa.zakaria@ejust.edu.eg>
2023-11-09 02:45:23 +0200

    input output

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-def write_file(filename="", text=""):
+def append_write(filename="", text=""):
     """ reads filename with utf-8 """
     with open(filename, "a", encoding= 'utf-8') as f:
         return f.write(text)
