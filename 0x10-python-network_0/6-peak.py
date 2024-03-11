8f6a90ba45fb117d95cc09834135469a019b5db0
AuroraSan <arwa.zakaria@ejust.edu.eg>
2024-03-11 13:36:56 +0200

    tasks search algo

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -15,10 +15,17 @@
 
 
 def find_peak(list_of_integers):
-    """BRUTE force implementation for question
-    """
-    max_i = None
-    for ele in list_of_integers:
-        if max_i is None or max_i < ele:
-            max_i = ele
-    return max_i
+    """Find a peak in a list of unsorted integers."""
+    if not list_of_integers:
+        return None
+
+    low, high = 0, len(list_of_integers) - 1
+
+    while low < high:
+        mid = (low + high) // 2
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            low = mid + 1
+        else:
+            high = mid
+
+    return list_of_integers[low]
