47039ca6885743686209d17c7a54ea8371791beb
AuroraSan <arwa.zakaria@ejust.edu.eg>
2024-03-11 13:29:36 +0200

    network tasks- fix errors

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -s -o /dev/null -w "%{http_code}\n" $1 | grep 200 && curl -sX GET $1
+# shellcheck disable=SC2046
+if [ $(curl -L -s -X HEAD -w "%{http_code}" "$1") == '200' ]; then curl -Ls "$1"; fi
